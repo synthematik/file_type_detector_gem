2c179c0bd311d86d8d53947f3bfc9f5446f0171b
Garik <garfild_2003@mail.ru>
2024-03-18 09:23:39 +0300

    Prototype

--- lib/ruby_gem.rb
+++ lib/ruby_gem.rb
@@ -6,3 +6,20 @@ module RubyGem
   class Error < StandardError; end
   # Your code goes here...
 end
+
+def detect_file_type(file_path)
+  File.open(file_path, 'rb') do |file|
+    header = file.read(4)
+    if header.start_with?('%PDF')
+      return 'PDF'
+    elsif header.start_with?('GIF8')
+      return 'GIF'
+    elsif header.start_with?("\x89PNG\r\n\x1A\n")
+      return 'PNG'
+    elsif header.start_with?("\xFF\xD8\xFF")
+      return 'JPEG'
+    else
+      return 'Неизвестный тип файла'
+    end
+  end
+end
