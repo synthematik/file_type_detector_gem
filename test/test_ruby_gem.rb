9db72f3a474f5e37aebc4f84c97982b1ad5b7e0d
watermelon0guy <55347575+watermelon0guy@users.noreply.github.com>
2024-04-01 10:29:43 +0300

    created special folders for each res types, rename tests

--- test/test_ruby_gem.rb
+++ test/test_ruby_gem.rb
@@ -1,58 +1,37 @@
 # frozen_string_literal: true
 
-require './test/test_helper'
+require "./test/test_helper"
 
-class TestFileTypeDetectors < Minitest::Test
+class TestPDFCheck < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::FileTypeDetector::VERSION
   end
 
-  def setup
-    @file_path = "test_file.pdf"
-    File.write(@file_path, "%PDF-12345 Some content here")
-  end
-
-  def teardown
-    File.delete(@file_path) if File.exist?(@file_path)
-  end
-
-  def test_raise_err_on_nonexistent_file
-    non_existent_file_path = "nonexistent_file.pdf"
-
-    assert_raises(IOError) { FileTypeDetector.check(non_existent_file_path) }
-  end
-
-  def test_detect_pdf
-    assert FileTypeDetector.check(@file_path)
-  end
-
-  def test_detect_non_pdf
-    File.write(@file_path, "This is not a PDF content")
-
-    refute FileTypeDetector.check(@file_path)
-  end
-
   def test_real_pdf
-    assert FileTypeDetector.check("test/test_res/real_pdf.pdf")
+    assert FileTypeDetector.check("test/test_resources/test_pdf_resources/real_pdf.pdf")
   end
 
   def test_fake_pdf
-    refute FileTypeDetector.check("test/test_res/real_png.pdf")
+    refute FileTypeDetector.check("test/test_resources/test_pdf_resources/real_png.pdf")
+  end
+
+  def test_not_pdf
+    refute FileTypeDetector.docx_check("test/test_resources/test_png_resources/real_png.png")
   end
 end
 
 
-class TestDocxFile < Minitest::Test
+class TestDocxCheck < Minitest::Test
   def test_real_docx
-    assert FileTypeDetector.docx_check("test/test_res/test_docx_resources/real_docx.docx")
+    assert FileTypeDetector.docx_check("test/test_resources/test_docx_resources/real_docx.docx")
   end
 
   def test_fake_docx
-    refute FileTypeDetector.docx_check("test/test_res/test_docx_resources/fake_docx.docx")
+    refute FileTypeDetector.docx_check("test/test_resources/test_docx_resources/fake_docx.docx")
   end
 
   def test_not_docx
-    refute FileTypeDetector.docx_check("test/test_res/test_png_resources/real_png.png")
+    refute FileTypeDetector.docx_check("test/test_resources/test_png_resources/real_png.png")
   end
 
 end
